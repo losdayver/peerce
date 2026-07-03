e09bd2e185b15d362ef33545da8b54fb9f0b80cd
olegzh <losdayver@yandex.ru>
2026-07-03 23:10:39 +0300

    simpleRelay listen all

--- src/simple/testExec/indexRelay.ts
+++ src/simple/testExec/indexRelay.ts
@@ -1,4 +1,4 @@
 import { TransceiverIPv4 } from "@src/transport/transceiver";
 import { SimpleRelay } from "../simpleRelay";
 
-const simpleRelay = new SimpleRelay("127.0.0.1", 5555);
+const simpleRelay = new SimpleRelay("0.0.0.0", 5555);
